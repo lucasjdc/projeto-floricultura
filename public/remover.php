d3fd7aec81fdacff9369eaf3cb216bd5cc487667
lucasjdc <lucasjpdc@gmail.com>
2025-08-21 00:46:41 -0300

    feat: adiciona sistema completo de controle de estoque para floricultura

--- public/remover.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/funcoes.php';
-
-if (isset($_GET['i'])) {
-    $estoque = carregarEstoque();
-    array_splice($estoque, intval($_GET['i']), 1);
-    salvarEstoque($estoque);
-}
-header("Location: index.php");
